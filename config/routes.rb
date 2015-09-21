d376dbef8d6c46dc860920859485876c7bc33a98
Makarand Pande <mak_pande@yahoo.com>
2015-09-20 21:49:37 -0400

    Assignment-32 completed but no advertisement instatnces added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
+  # get 'advertisements/index'
+  #
+  # get 'advertisements/show'
+  #
+  # get 'advertisements/new'
+  #
+  # get 'advertisements/create'
+
   resources :posts
+  resources :advertisements
   # get 'posts/index'
   #
   # get 'posts/show'
