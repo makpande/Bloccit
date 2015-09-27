5a59f6e0de98d2226b68511ff6dd71099855f108
Makarand Pande <mak_pande@yahoo.com>
2015-09-27 12:32:26 -0400

    assignment-38 complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,16 @@
+# create_table "topics", force: :cascade do |t|
+#   t.string   "name"
+#   t.boolean  "public",      default: true
+#   t.text     "description"
+#   t.datetime "created_at",                 null: false
+#   t.datetime "updated_at",                 null: false
+# end
+
 class Topic < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
   has_many :sponsored_posts, dependent: :destroy
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
 
 end
