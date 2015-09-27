5d143277d4a70e0dd28a6fabe25a34e519646f07
Makarand Pande <mak_pande@yahoo.com>
2015-09-27 18:56:24 -0400

    Checkpoint-39 user model

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
     resources :sponsored_posts, except: [:index]
   end
-
+  resources :users, only: [:new, :create]
   # resources :topics do
   #   resources :sponsored_posts, except: [:index]
   # end
