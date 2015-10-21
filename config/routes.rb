434ccf42d0c0e8fbdfaa95acd501fc0348c56405
Makarand Pande <mak_pande@yahoo.com>
2015-10-21 14:45:33 -0400

    assignment

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
   namespace :api do
      namespace :v1 do
        resources :users, only: [:index, :show, :create, :update]
-       resources :topics, except: [:edit, :new]
+       resources :topics, excepts: [:edit, :new]
        resources :comments, only: [:index, :show]
        resources :posts, only: [:index, :show]
      end
--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -106,7 +106,7 @@ RSpec.describe Api::V1::TopicsController, type: :controller do
 
    describe "DELETE destroy" do
        before { delete :destroy, id: my_topic.id }
- 
+
        it "returns http success" do
          expect(response).to have_http_status(:success)
        end
