8f886e481dc67d0c56c32b649727589baa1132ff
Makarand Pande <mak_pande@yahoo.com>
2015-10-29 12:56:34 -0400

    votes route mismatch

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
 
-  resources :post, only: [] do
+  resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
     post '/up-vote' => 'votes#up_vote', as: :up_vote
